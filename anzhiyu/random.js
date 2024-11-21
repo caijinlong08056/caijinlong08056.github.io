190f5e8d8c8c7e964688877dec957017cafed9c9
caijinlong08056 <723394257@qq.com>
2024-11-21 15:27:15 +0800

    Site updated: 2024-11-21 15:27:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/21/这是另一篇新的博文2024-11-21/","2024/11/19/hello-world/","2024/11/19/这是第一篇博文/"];function toRandomPost(){
+var posts=["2024/11/19/这是第一篇博文/","2024/11/21/这是另一篇新的博文2024-11-21/","2024/11/19/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

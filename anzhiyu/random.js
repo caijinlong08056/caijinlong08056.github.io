d9892102d1168e1348993a028b85fa494265eb08
caijinlong08056 <723394257@qq.com>
2024-11-24 00:07:00 +0800

    Site updated: 2024-11-24 00:07:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2024/11/21/这是另一篇新的博文2024-11-21/","2024/11/23/啄木鸟开店/","2024/11/23/小雪/"];function toRandomPost(){
+var posts=["2024/11/21/这是另一篇新的博文2024-11-21/","2024/11/24/过年/","2024/11/23/啄木鸟开店/","2024/11/23/小雪/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
